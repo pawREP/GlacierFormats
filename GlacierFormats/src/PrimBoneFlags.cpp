1bfb4708ae44fb20417076270de93bf2cdee5662
pawREP <pawREP@users.noreply.github.com>
2020-02-15 13:17:13 +0100

    Change class name

--- GlacierFormats/src/PrimBoneFlags.cpp
+++ GlacierFormats/src/PrimBoneFlags.cpp
@@ -4,16 +4,16 @@
 
 using namespace GlacierFormats;
 
-	BoneFlags::BoneFlags(int size) noexcept {
+	VertexColors::VertexColors(int size) noexcept {
 		flags = std::vector<unsigned char>(size, 0xFF);
 	}
 
-	BoneFlags::BoneFlags(BinaryReader* br, const SPrimSubMesh* prim_submesh) {
+	VertexColors::VertexColors(BinaryReader* br, const SPrimSubMesh* prim_submesh) {
 		auto size = 4 * prim_submesh->num_vertex;
 		flags.resize(size);
 		br->read(flags.data(), size);
 	}
 
-	void BoneFlags::serialize(BinaryWriter* bw) const {
+	void VertexColors::serialize(BinaryWriter* bw) const {
 		bw->write(flags.data(), flags.size());
 	};
