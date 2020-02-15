d9278c821c182dd4256f2b226944ae3be8fd3c6c
pawREP <pawREP@users.noreply.github.com>
2020-02-15 15:52:48 +0100

    Add documentation to prim seri types

--- GlacierFormats/src/PrimSerializationTypes.h
+++ GlacierFormats/src/PrimSerializationTypes.h
@@ -4,16 +4,15 @@ namespace GlacierFormats {
 
 #pragma pack(push,1)
 	struct SPrimHeader {
-		enum EPrimType : short {
 		enum class EPrimType : short {
 			PTOBJECTHEADER = 1,
 			PTMESH = 2,
 			PTSHAPE = 5,
 		};
 
-		char draw_destination;
-		char pack_type;
-		EPrimType type;
+		char draw_destination;	//Unused? Always 0
+		char pack_type;			//Unused? Always 0
+		EPrimType type;			//Prim type.
 	};
 
 	struct SPrims : public SPrimHeader {
@@ -21,7 +20,6 @@ namespace GlacierFormats {
 	};
 
 	struct SPrimObjectHeader : public SPrims {
-		enum PROPERTY_FLAGS {
 		enum class PROPERTY_FLAGS {
 			HAS_BONES = 0x01,
 			HAS_FRAMES = 0x02,
@@ -31,48 +29,47 @@ namespace GlacierFormats {
 			HAS_HIRES_POSITIONS = 0x0200,
 		};
 
-		PROPERTY_FLAGS property_flags;
-		int bone_rig_resource_index;
-		int num_objects;
-		int object_table;
-		float min[3];
-		float max[3];
+		PROPERTY_FLAGS property_flags;	//Properties
+		int bone_rig_resource_index;	//Index of BORG resource. Either -1 or 0. There are no meshes with multple BORG resources. (TODO: Needs verification by full scan.)
+		int num_objects;				//Number of mesh objects in the object table
+		int object_table;				//Offset to table of mesh objects
+		float min[3];					//Global bounding box min
+		float max[3];					//Global bounding box max
 	};
 
 	struct SPrimObject : public SPrimHeader {
 
-		enum SUBTYPE : char {
-			SUBTYPE_STANDARD = 0,
-			SUBTYPE_LINKED = 1,
-			SUBTYPE_WEIGHTED = 2,
-			SUBTYPE_STANDARDUV2 = 3,
-			SUBTYPE_STANDARDUV3 = 4,
-			SUBTYPE_STANDARDUV4 = 5,
-			SUBTYPE_SPEEDTREE = 6,
 		enum class SUBTYPE : char {
+			SUBTYPE_STANDARD = 0,		//Static mesh
+			SUBTYPE_LINKED = 1,			//Rigged mesh
+			SUBTYPE_WEIGHTED = 2,		//Rigged and skinned mesh
+			SUBTYPE_STANDARDUV2 = 3,	//Unused
+			SUBTYPE_STANDARDUV3 = 4,	//Unused
+			SUBTYPE_STANDARDUV4 = 5,	//Unused
+			SUBTYPE_SPEEDTREE = 6,		//Meshes generate by SpeedTree middleware
 		};
 
 		enum class PROPERTY_FLAGS : char {
-			PROPERTY_XAXISLOCKED = 0x01,
+			PROPERTY_XAXISLOCKED = 0x01,	
 			PROPERTY_YAXISLOCKED = 0x02,
 			PROPERTY_ZAXISLOCKED = 0x04,
-			PROPERTY_HIRES_POSITIONS = 0x08,
+			PROPERTY_HIRES_POSITIONS = 0x08,	//Set for meshes that use 32 bit precision position data, instead of the default 16 bit precision.
 			PROPERTY_PS3_EDGE = 0x10,
-			PROPERTY_COLOR1 = 0x20,
+			PROPERTY_COLOR1 = 0x20,				//Set for meshes that do NOT contain vertex color data.
 			PROPERTY_ISNOPHYSICSPROP = 0x40,
 		};
 
-		SUBTYPE sub_type; //PRIM_OBJECT_SUB_TYPE
-		PROPERTY_FLAGS properties;
-		unsigned char lod_mask;
-		char variant_id;
+		SUBTYPE sub_type;				//Object subtype
+		PROPERTY_FLAGS properties;		//Properties
+		unsigned char lod_mask;			//Lod mask. One bit per distance range. Lower bits indicate closer ranges.
+		char variant_id;				//Variant id. Used for models that have multiple variants that share the same mesh, like miami race cars in Hitman 2.
 		char bias;
 		char offset;
-		short material_id;
-		int wire_color; //color used by wire-frame debug render.
-		int unk2;
-		float min[3];
-		float max[3];
+		short material_id;				//Index of the MATI reference used for this mesh
+		int wire_color;					//Color used by wire-frame debug render.
+		int unk2;						//Unknown. Assert(0);
+		float min[3];					//Bounding box min
+		float max[3];					//Bounding box max
 	};
 
 	struct SPrimMesh : public SPrimObject {
@@ -80,23 +77,19 @@ namespace GlacierFormats {
 		enum class CLOTH_FLAGS {
 			//...
 			USE_SMOLL_CLOTH_BLOCK = 0x80 //When set, indicates that the submesh uses the small cloth block variant instead
-										 // of the large, 0x14 bytes per vertex one. Check PrimClothData.cpp for details.
+										 //of the large, 0x14 bytes per vertex one. Check PrimClothData.cpp for details.
 			//...
 		};
 
-		int sub_mesh_table;
-		float pos_scale[4];
-		float pos_bias[4];
-		float uv_scale[2];
-		float uv_bias[2];
-		CLOTH_FLAGS cloth_flags;
-		float unk17;
+		int sub_mesh_table;			//Offset, yable of submeshes.
+		float pos_scale[4];			//Scale used to decompress vertex position data
+		float pos_bias[4];			//Bias used to decompress vertex position data
+		float uv_scale[2];			//Scale used to decompress uv coordinates
+		float uv_bias[2];			//Bias used to decompress uv coordinates
+		CLOTH_FLAGS cloth_flags;	//Bitfield used for cloth properties. 
+		int unk17;					
 		int m_unk_tabl1_offset;
 		int m_unk_tabl2_offset;
-	};
-
-	struct SPrimMeshLinked : public SPrimMesh {
-		unsigned int link_table;
 	};
 
 	struct SPrimMeshWeighted : public SPrimMesh {
@@ -107,15 +100,15 @@ namespace GlacierFormats {
 	};
 
 	struct SPrimSubMesh : SPrimObject {
-		int num_vertex;
-		int vertex_buffer;
-		int num_indices;
-		int num_indices_ex;//Number of additional indices in index buffer extension. 
-		int index_buffer;
-		int collision;
-		int cloth;
-		int num_uv_channels;
-		int sm_unk_tabl2_offset;
+		int num_vertex;				//Number of vertices in the submesh
+		int vertex_buffer;			//Offest to vertex buffer
+		int num_indices;			//Number of indices in index buffer
+		int num_indices_ex;			//Number of additional indices in index buffer extension. This is rarely used in meshes with very large index buffers. ~10/30000
+		int index_buffer;			//Offset of index buffer
+		int collision;				//Offset to collision data
+		int cloth;					//Offset to cloth data
+		int num_uv_channels;		//Number of UV channels. Very few meshes use more than one. ~20/30000
+		int sm_unk_tabl2_offset;	
 	};
 
 #pragma pack(pop)
