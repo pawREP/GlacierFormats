9d4a5bd0f582eb14f1c939321a2b2cfc48b22d23
pawREP <pawREP@users.noreply.github.com>
2020-04-16 02:06:39 +0200

    Fix bug where runtime id wouldn't be set in TEXT ctor

--- GlacierFormats/src/TEXT.cpp
+++ GlacierFormats/src/TEXT.cpp
@@ -2,6 +2,7 @@
 #include "TEXD.h"
 #include "BinaryReader.hpp"
 #include "BinaryWriter.hpp"
+#include "ResourceRepository.h"
 
 using namespace GlacierFormats;
 
@@ -15,6 +16,10 @@ TEXT::TEXT(const TEXT& other) : TextureResource<TEXT>(other) {
 
 TEXT::TEXT(const TEXD& other) : TextureResource<TEXT>() {
 	initializeTextFromTexd(other, *this);
+
+	auto parent_text_id = ResourceRepository::instance()->getResourceBackReferences(other.id, "TEXT");
+	GLACIER_ASSERT_TRUE(parent_text_id.size() == 1);// TEXDs only have one TEXT parent. If this ever fails, check if it's one of those cases where the repo contains duplicate files.
+	id = parent_text_id.front();
 }
 
 TEXT::TEXT(BinaryReader& br, RuntimeId id) : TextureResource<TEXT>(br, id) {
