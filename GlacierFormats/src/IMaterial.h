c091821b5c92e8c7a8c804168dc8c35d32f5c1f6
pawREP <pawREP@users.noreply.github.com>
2020-04-21 21:37:30 +0200

    Prune interface

--- GlacierFormats/src/IMaterial.h
+++ GlacierFormats/src/IMaterial.h
@@ -9,13 +9,13 @@ namespace GlacierFormats {
 	class IMaterial {
 	public:
 		virtual int getTextureCount() const = 0;
-		virtual std::vector<std::string> getTextureNames() const = 0;
+		//virtual std::vector<std::string> getTextureNames() const = 0;
 
 		virtual std::unique_ptr<TEXD> getTextureResourceByIndex(int idx) const = 0;
 
-		virtual std::unique_ptr<TEXD> getDiffuseMap() const = 0;
-		virtual std::unique_ptr<TEXD> getNormalMap() const = 0;
-		virtual std::unique_ptr<TEXD> getSpecularMap() const = 0;
+		//virtual std::unique_ptr<TEXD> getDiffuseMap() const = 0;
+		//virtual std::unique_ptr<TEXD> getNormalMap() const = 0;
+		//virtual std::unique_ptr<TEXD> getSpecularMap() const = 0;
 
 	};
 }
